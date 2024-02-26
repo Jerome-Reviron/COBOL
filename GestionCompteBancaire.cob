5faa79276ad032513ce33b0f837ba2fc46872ff7
Jerome-Reviron <jrinstitut63@hotmail.fr>
2024-02-26 15:42:05 +0100

    GestionCompteBancaire

---
+++ GestionCompteBancaire.cob
@@ -0,0 +1,75 @@
+IDENTIFICATION DIVISION.
+PROGRAM-ID. GestionCompteBancaire.
+
+DATA DIVISION.
+WORKING-STORAGE SECTION.
+   01 SoldeCompte PIC 9(5)V99 VALUE 1000.00.
+   01 Montant PIC 9(5)V99 VALUE 0.
+   01 SoldeAutreCompte PIC 9(5)V99 VALUE 500.00.
+   01 ChoixUtilisateur PIC 9 VALUE 0.
+
+PROCEDURE DIVISION.
+
+   PERFORM AFFICHER-MENU
+   PERFORM UNTIL ChoixUtilisateur = 0
+       PERFORM TRAITER-CHOIX
+       PERFORM AFFICHER-MENU
+   END-PERFORM.
+
+   DISPLAY "Merci d'avoir utilisé notre service. Au revoir!"
+   STOP RUN.
+
+AFFICHER-MENU.
+   DISPLAY "Solde actuel du compte : " SoldeCompte
+   DISPLAY "Solde actuel de l'autre compte : " SoldeAutreCompte
+   DISPLAY "Opérations disponibles :"
+   DISPLAY "1. Dépôt"
+   DISPLAY "2. Retrait"
+   DISPLAY "3. Virement"
+   DISPLAY "4. Afficher le solde"
+   DISPLAY "0. Quitter"
+   ACCEPT ChoixUtilisateur.
+
+TRAITER-CHOIX.
+   EVALUATE ChoixUtilisateur
+      WHEN 1
+         PERFORM DEPOT
+      WHEN 2
+         PERFORM RETRAIT
+      WHEN 3
+         PERFORM VIREMENT
+      WHEN 4
+         PERFORM AFFICHER_SOLDE
+      WHEN 0
+         CONTINUE
+      WHEN OTHER
+         DISPLAY "Choix invalide. Veuillez choisir 1, 2, 3, 4 ou 0."
+   END-EVALUATE.
+
+DEPOT.
+   DISPLAY "Entrez le montant du dépôt : ".
+   ACCEPT Montant.
+   COMPUTE SoldeCompte = SoldeCompte + Montant.
+
+RETRAIT.
+   DISPLAY "Entrez le montant du retrait : ".
+   ACCEPT Montant.
+   IF Montant > SoldeCompte
+      DISPLAY "Solde insuffisant. Opération annulée."
+   ELSE
+      COMPUTE SoldeCompte = SoldeCompte - Montant
+   END-IF.
+
+VIREMENT.
+   DISPLAY "Entrez le montant du virement : ".
+   ACCEPT Montant.
+   IF Montant > SoldeCompte
+      DISPLAY "Solde insuffisant. Virement annulé."
+   ELSE
+      COMPUTE SoldeCompte = SoldeCompte - Montant
+      COMPUTE SoldeAutreCompte = SoldeAutreCompte + Montant
+   END-IF.
+
+AFFICHER_SOLDE.
+   DISPLAY "Solde actuel du compte : " SoldeCompte.
+   DISPLAY "Solde actuel de l'autre compte : " SoldeAutreCompte.
